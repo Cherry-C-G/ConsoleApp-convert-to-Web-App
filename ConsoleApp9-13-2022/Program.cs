12a6c1474a1b53eb6ac962e1ce19f2b03319acbd
Xin Gao <celiagao830@gmail.com>
2022-09-13 19:25:01 -0500

    first commit

---
+++ ConsoleApp9-13-2022/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp9_13_2022;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+    }
+}
---
+++ ConsoleApp9-13-2022/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9_13_2022
+{
+    internal class Startup
+    {
+        public void ConfigurationServices(IServiceCollection service)
+        {
+
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World");
+                });
+            });
+        }
+    }
+
+}
